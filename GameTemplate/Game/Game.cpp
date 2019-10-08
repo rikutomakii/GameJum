2871899a939e83d3a2bc9aa2f5e7bf1250a231b1
槙井 <kbc18b25@stu.kawahara.ac.jp>
2019-10-08 14:32:49 +0900

    kickoff

---
+++ GameTemplate/Game/Game.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Game.h"
+#include "tkEngine/light/tkDirectionLight.h"
+
+Game::Game()
+{
+}
+
+
+Game::~Game()
+{
+}
+bool Game::Start()
+{	
+	return true;
+}
+
+void Game::Update()
+{
+}
