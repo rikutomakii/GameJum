5fbd9ac796c1da6c593ba7dc41c2c7ca4e46c45a
槙井 <kbc18b25@stu.kawahara.ac.jp>
2019-10-09 17:20:35 +0900

    ドア更新

--- GameTemplate/Game/Door.cpp
+++ GameTemplate/Game/Door.cpp
@@ -17,36 +17,25 @@ bool Door::Start()
 	m_skinModelRender2 = NewGO<prefab::CSkinModelRender>(0);
 	m_skinModelRender2->Init(L"modeldata/Door2.cmo");
 
-	//m_skinModelRender2->SetPosition({ 70.0f,0.0f,0.0f });
+	m_skinModelRender->SetPosition({ 70.0f,0.0f,0.0f });
+	m_skinModelRender2->SetPosition({ -70.0f,0.0f,0.0f });
 
+	//m_skinModelRender2->SetPosition({ 70.0f,0.0f,0.0f });
+	
 	return true;
 }
 
 void Door::Update()
 {
-	if (DoorFlag == false/*&& m_position.x ==70&& m_position2.x == -70*/ )
-	{
-
-		/*m_position.x = 0.05f;
-		m_position2.x = -0.05f;*/
-
-		/*m_skinModelRender->SetPosition(m_position);
-		m_skinModelRender2->SetPosition(m_position2);*/
-
-		m_skinModelRender->SetPosition({ 70.0f,0.0f,0.0f });
-		m_skinModelRender2->SetPosition({ -70.0f,0.0f,0.0f });
-		DoorFlag = true;
-	}
-
-
 	
-	//m_timer++;
-	if (Pad(0).IsPressAnyKey()) {
+	
+
+	/*if (Pad(0).IsPressAnyKey()) {
 		m_position.x = -0.1f;
 		m_position2.x = 0.1f;
 		m_skinModelRender->SetPosition(m_position);
 		m_skinModelRender2->SetPosition(m_position2);
-		DoorFlag = false;
-	}
+		
+	}*/
 	
 }
--- GameTemplate/Game/Door.h
+++ GameTemplate/Game/Door.h
@@ -14,5 +14,7 @@ public:
 	CVector3 m_position2 = CVector3::Zero;
 	float m_timer = 0.0f;
 	bool DoorFlag = false;
+	bool DoorFlag2 = false;
+	
 };
 
