da88189a0fcbcf9b41a9946c90b05b61dd5d3fff
炎崎双静 <kbc19b32@stu.kawahara.ac.jp>
2019-10-09 16:16:33 +0900

    ゲーム制作　16：15 先輩、来栖待ち

--- GameTemplate/Game/result.cpp
+++ GameTemplate/Game/result.cpp
@@ -1,5 +1,5 @@
-#include "result.h"
 #include "stdafx.h"
+#include "result.h"
 
 
 result::result()
@@ -9,4 +9,22 @@ result::result()
 
 result::~result()
 {
+	//スプライトレンダラーを削除。
+	DeleteGO(m_spriteRender);
+}
+
+bool result::Start()
+{
+	//CSpriteRenderのインスタンスを生成
+	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
+	m_spriteRender->Init(L"sprite/gameClear.dds",		//読み込むファイルのファイルパス。
+		1105.0f,					//スプライトの幅。
+		115.0f						//スプライトの高さ。
+	);
+	return true;
+}
+
+void result::Update()
+{
+
 }
--- GameTemplate/Game/result.h
+++ GameTemplate/Game/result.h
@@ -6,5 +6,10 @@ public:
 	result();
 	~result();
 
+	bool Start();
+
+	void Update();
+
+	prefab::CSpriteRender* m_spriteRender = nullptr;
 
 };
