753cd9be5bac1442728f2d5e135baa5492ba6149
炎崎双静 <kbc19b32@stu.kawahara.ac.jp>
2019-10-11 10:53:04 +0900

    ドアの移動　完成

--- GameTemplate/Game/Titlescene.cpp
+++ GameTemplate/Game/Titlescene.cpp
@@ -12,6 +12,9 @@ Titlescene::~Titlescene()
 {
 
 	DeleteGO(m_spriteRender);
+	DeleteGO(R_spriteRender);
+	DeleteGO(L_spriteRender);
+	DeleteGO(m_spriteRender);
 	DeleteGO(m_bgmSoundSource);
 	DeleteGO(m_cfontrender);
 }
@@ -19,8 +22,20 @@ Titlescene::~Titlescene()
 bool Titlescene::Start()
 {
 	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
+	R_spriteRender = NewGO<prefab::CSpriteRender>(0);
+	L_spriteRender = NewGO<prefab::CSpriteRender>(0);
 	//タイトル画面をロード
-	m_spriteRender->Init(L"sprite/famima.dds", 1280.0f, 720.0f);
+	m_spriteRender->Init(L"sprite/famima ao tobira waku.dds", 1000.0f, 720.0f);
+	//ドアの右左の画像をロード
+	R_spriteRender->Init(L"sprite/famima ao tobira migi.dds", 300.0f, 350.0f);
+	L_spriteRender->Init(L"sprite/famima ao tobira hidari.dds", 300.0f, 350.0f);
+
+	//右側のドア
+	R_position.x = 150;
+	R_position.y = -185;
+	//左側のドア
+	L_position.x = -150;
+	L_position.y = -185;
 	
 
 	m_cfontrender = NewGO<prefab::CFontRender>(0);
@@ -55,9 +70,22 @@ void Titlescene::Update()
 
 	//もしもAボタンが押されたら
 	if (Pad(0).IsPressAnyKey()) {
+
+		//ドアが開いているかの判定
+		IsOpendoor = true;
+		
+	}
+	//ドアが開いているときに移動する
+	if (IsOpendoor == true) {
+		R_position.x += 15;
+		L_position.x -= 15;
+	}
+
+	//ドアが止まる判定
+	if (R_position.x > 300 && L_position.x < -300) {
 		//GameSceneのインスタンスを生成
 		NewGO<Game>(0, "Game");
-		
+
 		//GameSceneのインスタンスを生成
 		ss = NewGO<prefab::CSoundSource>(0);
 
@@ -66,7 +94,11 @@ void Titlescene::Update()
 
 		//シーンが切り替わるのでTitlsSceneのインスタンスを削除
 		DeleteGO(this);
-		
+		IsOpendoor = false;
 	}
+
+	//ドアのポジション
+	R_spriteRender->SetPosition(R_position);
+	L_spriteRender->SetPosition(L_position);
 
 }
